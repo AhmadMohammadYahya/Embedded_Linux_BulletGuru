76041366ff3ff4c28503a0338d700ab2777464c9
AhmadMohammadYahya <Ahmad.Mohammad.Yahya@gmail.com>
2024-11-11 15:37:29 +0100

    Update BuggyApp.cpp

--- BuggyApp.cpp
+++ BuggyApp.cpp
@@ -2,12 +2,17 @@
 #include <fstream>
 #include <string>
 
-/* Solve problem in 
+/* problem 1
 arch_prctl(0x3001 // ARCH_??? //, 0x7ffe2ca56c80) = -1 EINVAL (Invalid argument)
 */
-#include <asm/prctl.h>        /* Definition of ARCH_* constants */
-#include <sys/syscall.h>      /* Definition of SYS_* constants */
-#include <unistd.h>
+
+/* problem 2
+access("/etc/ld.so.preload", R_OK)      = -1 ENOENT (No such file or directory)
+*/
+
+/* problem 3
+openat(AT_FDCWD, "sample.txt", O_RDONLY) = -1 ENOENT (No such file or directory)
+*/
 
 int main() {
 
